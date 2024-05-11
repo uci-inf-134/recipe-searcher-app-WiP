a3bb64354683b7f6cc4874a3fa86e083770bc7ab
Quamin Hua <quaminhua56@gmail.com>
2024-05-10 19:56:00 -0700

    created new recipe page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FridgeComponent } from './fridge/fridge.component';
+import { RecipePageComponent } from './recipe-page/recipe-page.component';
 
 export const routes: Routes = [
     { path: '', component: FridgeComponent }, 
+    { path: 'recipe/:recipeId', component: RecipePageComponent }
 ];
---
+++ src/app/recipe-page/recipe-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-page.component.html',
+  styleUrl: './recipe-page.component.css'
+})
+export class RecipePageComponent {
+
+}
