023a96bb427fee02716364a31164faf0e1418e0d
Quamin Hua <quaminhua56@gmail.com>
2024-05-11 20:37:27 -0700

    some routing changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FridgeComponent } from './fridge/fridge.component';
+import { RecipePageComponent } from './recipe-page/recipe-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, FridgeComponent], 
+  imports: [CommonModule, RouterModule, FridgeComponent, RecipePageComponent], 
   templateUrl: './app.component.html', 
   styleUrls: ['./app.component.css'],
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Route } from '@angular/router';
 import { FridgeComponent } from './fridge/fridge.component';
 import { RecipePageComponent } from './recipe-page/recipe-page.component';
+import { AppComponent } from './app.component';
 
 export const routes: Route[] = [
-    { path: '', component: FridgeComponent }, 
-    { path: 'recipe/:recipeId', component: RecipePageComponent }
+    { path: '', component: AppComponent },
+    { path: 'fridge', component: FridgeComponent },
+    { path: 'recipe', component: RecipePageComponent }
 ];
