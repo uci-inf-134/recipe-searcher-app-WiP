37554d4925f7581a8cba2ebc65cc5c85fb5c27d4
Ashley Ly <skywings173@gmail.com>
2024-06-07 19:33:10 -0700

    added functionality to add item

--- src/app/fridge/fridge.component.ts
+++ src/app/fridge/fridge.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { Ingredient } from '../ingredient';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-fridge',
   templateUrl: './fridge.component.html',
   styleUrls: ['./fridge.component.css'],
-  imports: [NavbarComponent, CommonModule],
+  imports: [NavbarComponent, CommonModule, FormsModule],
   standalone: true
   
 })
@@ -16,8 +17,22 @@ export class FridgeComponent {
   testIngredient:Ingredient= new Ingredient("carrot", "a long carrot", "vegetable", 10, new Date("11-10-24"));
   allIngredients:Ingredient[] = [this.testIngredient];
 
-  addIngredient(item: Ingredient) //adds an ingredient to the fridge
+  //temporary variables for form input
+  name: string = "";
+  details: string = "";
+  type: string = "";
+  exDate:string = "";
+  amount:number = 0;
+  
+  
+
+
+
+  
+  addIngredient() //adds an ingredient to the fridge
   {
+    var temp = new Date(this.exDate);
+    let item = new Ingredient(this.name, this.details, this.type, this.amount, temp);
     this.allIngredients.push(item);
   }
 
